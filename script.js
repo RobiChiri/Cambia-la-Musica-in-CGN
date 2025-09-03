3965c61823a9382ce18748abde458a49f767492b
Simo <simone.florea@cgn.it>
2025-09-03 13:14:28 +0200

    errori

---
+++ script.js
@@ -0,0 +1,13 @@
+var miaVar=1;
+
+function Mostra(){
+    if(miaVar===1){
+        document.get
+        getElementsByClassName("listaF").style.visibility="visible";
+        miaVar=0;
+    }
+    else if(miaVar===0){
+        document.getElementsByClassName("listaF").style.visibility="hidden";
+        miaVar=1;
+    }
+}
