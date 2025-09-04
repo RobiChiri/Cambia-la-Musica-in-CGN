57a0e3c65c9c2dd7779d7eb151aa1be5d91812bb
anas <anastasia.pustelnyk@cgn.it>
2025-09-04 09:05:53 +0200

    funzione toggle footer

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var miaVar=1;
 
 function Mostra(){
     if(miaVar===1){
-        document.get
-        getElementsByClassName("listaF").style.visibility="visible";
+        document.getElementById("listaF").style.visibility="visible";
+        document.getElementById("listaC").style.visibility="visible";
+        document.getElementById("listaD").style.visibility="visible";
         miaVar=0;
     }
     else if(miaVar===0){
-        document.getElementsByClassName("listaF").style.visibility="hidden";
+        document.getElementById("listaF").style.visibility="hidden";
+        document.getElementById("listaC").style.visibility="hidden";
+        document.getElementById("listaD").style.visibility="hidden";
         miaVar=1;
     }
 }
