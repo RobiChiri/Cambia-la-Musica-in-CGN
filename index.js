308c78b959f595b47901703a17ac444ccef84f43
FernNardillo <fernando.nardillo@cgn.it>
2025-09-05 10:20:51 +0200

    aggiunto completamente quiz, manca pulsante nella navbar

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var a, b, c;
 
 function scegliDomande() {
 
-    const numdomande = 8;
+    const numdomande = 7;
 
     a = Math.round(Math.random() * numdomande);
     b = Math.round(Math.random() * numdomande);
@@ -21,38 +21,21 @@ function scegliDomande() {
 
 
 
-    while (a == b || a == c || b == c) {
+    while ((a == b || a == c || b == c) && (!a || !b || !c)) {
         a = Math.round(Math.random() * numdomande);
         b = Math.round(Math.random() * numdomande);
         c = Math.round(Math.random() * numdomande);
     }
 
-    /*while(a == b){
-        a = Math.round(Math.random() * numdomande);
-        b = Math.round(Math.random() * numdomande);
-    }
+    console.log(a + " " + b + " "+ c);
 
-    while(c == b){
-        c = Math.round(Math.random() * numdomande);
-        b = Math.round(Math.random() * numdomande);
-    }
+    var domanda1 = document.getElementById("domanda1");
+    var domanda2 = document.getElementById("domanda2");
+    var domanda3 = document.getElementById("domanda3");
 
-    while(c == a){
-        c = Math.round(Math.random() * numdomande);
-        a = Math.round(Math.random() * numdomande);
-    }*/
-    var domanda1;
-    var domanda2;
-    var domanda3;
-    setTimeout(() => {
-        domanda1 = document.getElementById("domanda1");
-        domanda2 = document.getElementById("domanda2");
-        domanda3 = document.getElementById("domanda3");
-        domanda1.textContent = domanda[a][0];
-        domanda2.textContent = domanda[b][0];
-        domanda3.textContent = domanda[c][0];
-    }, 200);
-
+    domanda1.textContent = domanda[a][0];
+    domanda2.textContent = domanda[b][0];
+    domanda3.textContent = domanda[c][0];
 }
 
 
@@ -61,46 +44,50 @@ function controllaRisultato() {
     const radioButtons2 = document.querySelectorAll('input[name="risposta2"]');
     const radioButtons3 = document.querySelectorAll('input[name="risposta3"]');
 
+    var Border1 = document.getElementById("domanda1").parentElement.style.borderColor;
+    var Border2 = document.getElementById("domanda2").parentElement.style.borderColor;
+    var Border3 = document.getElementById("domanda3").parentElement.style.borderColor;
+
     for (var i = 0; i < 2; i++) {
         if (radioButtons1[i].checked) {
             if ((radioButtons1[i].getAttribute('value') == "vero" && domanda[a][1]) || (radioButtons1[i].getAttribute('value') == "falso" && !domanda[a][1])) {
-                document.getElementById("domanda1").parentElement.style.borderColor = 'lightgreen';
+                Border1 = 'lightgreen';
                 /*alert("ciao1" + radioButtons1[i].getAttribute('value'));*/
             } else {
-                document.getElementById("domanda1").parentElement.style.borderColor = 'red';
+                Border1 = 'red';
             }
 
         } else {
-            if (i == 1 && document.getElementById("domanda1").parentElement.style.borderColor != 'lightgreen' && document.getElementById("domanda1").parentElement.style.borderColor != 'red') {
-                document.getElementById("domanda1").parentElement.style.borderColor = 'yellow';
+            if (i == 1 && Border1 != 'lightgreen' && Border1 != 'red') {
+                Border1 = 'yellow';
             }
         }
 
         if (radioButtons2[i].checked) {
             if ((radioButtons2[i].getAttribute('value') == "vero" && domanda[b][1]) || (radioButtons2[i].getAttribute('value') == "falso" && !domanda[b][1])) {
-                document.getElementById("domanda2").parentElement.style.borderColor = 'lightgreen';
+                Border2 = 'lightgreen';
                 /*alert("ciao2" + radioButtons2[i].getAttribute('value'));*/
             } else {
-                document.getElementById("domanda2").parentElement.style.borderColor = 'red';
+                Border2 = 'red';
             }
         } else {
-            if (i == 1 && document.getElementById("domanda2").parentElement.style.borderColor != 'lightgreen' && document.getElementById("domanda2").parentElement.style.borderColor != 'red') {
-                document.getElementById("domanda2").parentElement.style.borderColor = 'yellow';
+            if (i == 1 && Border2 != 'lightgreen' && Border2 != 'red') {
+                Border2 = 'yellow';
             }
 
         }
 
         if (radioButtons3[i].checked) {
             if ((radioButtons3[i].getAttribute('value') == "vero" && domanda[c][1]) || (radioButtons3[i].getAttribute('value') == "falso" && !domanda[c][1])) {
-                document.getElementById("domanda3").parentElement.style.borderColor = 'lightgreen';
+                Border3 = 'lightgreen';
                 /*alert("ciao3" + radioButtons3[i].getAttribute('value'));*/
             } else {
-                document.getElementById("domanda3").parentElement.style.borderColor = 'red';
+                Border3 = 'red';
             }
 
         } else {
-            if (i == 1 && document.getElementById("domanda3").parentElement.style.borderColor != 'lightgreen' && document.getElementById("domanda3").parentElement.style.borderColor != 'red') {
-                document.getElementById("domanda3").parentElement.style.borderColor = 'yellow';
+            if (i == 1 && Border3 != 'lightgreen' && Border3 != 'red') {
+                Border3 = 'yellow';
             }
 
         }
@@ -108,7 +95,7 @@ function controllaRisultato() {
     }
 
     var risultatoScritta = document.getElementById("risultato");
-    if (document.getElementById("domanda1").parentElement.style.borderColor == 'lightgreen' && document.getElementById("domanda2").parentElement.style.borderColor == 'lightgreen' && document.getElementById("domanda3").parentElement.style.borderColor == 'lightgreen') {
+    if (Border1 == 'lightgreen' && Border2 == 'lightgreen' && Border3 == 'lightgreen') {
         risultatoScritta.textContent = "BRAVO SEI PASSATO!";
         risultatoScritta.style.color = 'lightgreen';
         risultatoScritta.style.visibility = "visible";
